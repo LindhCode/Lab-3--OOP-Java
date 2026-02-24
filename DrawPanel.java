2071bae246789abfb67a72a257113578d54feb80
axellindh2004@gmail.com <axellindh2004@gmail.com>
2026-02-24 09:48:17 +0100

    Klar med VehicleAndMiscHandling

--- DrawPanel.java
+++ DrawPanel.java
@@ -25,18 +25,14 @@ public class DrawPanel extends JPanel{
 
         // Här läggs bilarna till
         // Här läggs alla bilar till, med bilobjekt, bild, point
-        cc.getVehicles().add(new VehicleData(new Volvo240(Color.black, 200), ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg")), new Point(100,200)));
-        cc.getVehicles().add(new VehicleData(new Saab95(Color.green, 200), ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab95.jpg")), new Point(200,200)));
-        cc.getMiscs().add(new MiscData(new MechanicShop<Volvo240>(10, 500, 200, "Volvo240MechanicShop"), ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg")), new Point(500, 200)));
-        cc.getVehicles().add(new VehicleData(new Scania(Color.black, 700), ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Scania.jpg")), new Point(300,200)));
 
         // Synkar fordonens x/y-Pos med Pointens x/y
-        for(VehicleData vehObj : cc.getVehicles()){
+        for(VehicleData vehObj : cc.getHandler().getVehicles()){
             vehObj.getVehicle().setxPos(vehObj.getPoint().x);
             vehObj.getVehicle().setyPos(vehObj.getPoint().y);
         }
         // Synkar för miscs
-        for (MiscData miscObj : cc.getMiscs()) {
+        for (MiscData miscObj : cc.getHandler().getMiscs()) {
             miscObj.getMiscObj().setxPos(miscObj.getPoint().x);
             miscObj.getMiscObj().setyPos(miscObj.getPoint().y);
         }
@@ -49,10 +45,10 @@ public class DrawPanel extends JPanel{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         // Updatera för alla i listan
-        for(VehicleData vehObj : cc.getVehicles()){
+        for(VehicleData vehObj : cc.getHandler().getVehicles()){
             g.drawImage(vehObj.getImage(),vehObj.getPoint().x,vehObj.getPoint().y, null);
         }
-        for (MiscData miscObj : cc.getMiscs()){
+        for (MiscData miscObj : cc.getHandler().getMiscs()){
             g.drawImage(miscObj.getImage(),miscObj.getPoint().x,miscObj.getPoint().y, null);
         }
     }
